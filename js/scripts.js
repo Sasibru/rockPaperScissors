2c43955ac578d7f96b50343aa6ad2529e1d87327
Sander Brustad <sander.simonsen.brustad@google.com>
2022-10-23 20:57:40 +0200

    Add WIP of step 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
-const choices = ["Rock", "Paper", "Scissors"];
+const choices = ["ROCK", "PAPER", "SCISSORS"];
 
 function getComputerChoice(choices) {
   let randomPick = Math.floor(Math.random() * choices.length);
   return choices[randomPick];
 }
+
+// jeg er paa steg 4
+
+function playRound(playerSelection, computerSelection) {
+  let capital = playerSelection.toUpperCase();
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice(choices);
