120f507b2d850e0900fe6a258417335fe8e855e4
Sander Brustad <sander.simonsen.brustad@google.com>
2022-10-31 13:33:52 +0100

    Add computer generated picks in the for loop

--- js/scripts.js
+++ js/scripts.js
@@ -41,42 +41,15 @@ function playRound(playerSelection, computerSelection) {
 // Auto play 5 rounds
 
 let autoPlay = '';
+let autoGenerateSelection = "";
 
 function game() {
   for (let i = 0; i < 5; i++) {
-    getComputerChoice(choices)
-    console.log(playRound(playerSelection, computerSelection));
+    autoGenerateSelection = getComputerChoice(choices);
+    console.log(playRound(playerSelection, autoGenerateSelection));
   }
   return autoPlay
 }
 
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
 console.log(game());
-
-
-
-///////////////////////////////////////////////////////////////////
-// New code
-
-// const choices = ["ROCK", "PAPER", "SCISSORS"];
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice(choices);
-
-// function getComputerChoice(choices) {
-//   let randomPick = Math.floor(Math.random() * choices.length);
-//   return choices[randomPick];
-// }
-
-// function allCapString(stringInput) {
-//   return stringInput.toUpperCase();
-// }
-
-// function playRound(playerSelection, computerSelection) {
-//   if (computerSelection === playerSelection){
-//     return "Draw, try again!"
-//   } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS" ||
-//              playerSelection === "PAPER" && computerSelection === "ROCK" ||
-//              playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-//     return "Player wins!"
-//   }
-// }
